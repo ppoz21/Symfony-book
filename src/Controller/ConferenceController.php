88b7311f939e6e9b563a7dab0577ed04c940b7b3
Paweł Palacz <ppalacz@edu.cdv.pl>
2020-06-18 15:19:33 +0200

    Added cache

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -33,13 +33,27 @@ class ConferenceController extends AbstractController
     }
 
     /**
+     * @Route("/conference_header", name="conference_header")
+     */
+    public function conferenceHeader(ConferenceRepository $conferenceRepository)
+    {
+        $response = new Response($this->twig->render('conference/index.html.twig',[
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+        $response->setSharedMaxAge(3600);
+        return $response;
+    }
+
+    /**
      * @Route("/", name="homepage")
      */
     public function index(ConferenceRepository $conferenceRepository)
     {
-        return new Response($this->twig->render('conference/index.html.twig',[
+        $response = new Response($this->twig->render('conference/index.html.twig',[
             'conferences' => $conferenceRepository->findAll(),
             ]));
+        $response->setSharedMaxAge(3600);
+        return $response;
     }
     /**
      * @Route("/conference/{slug}", name="conference")
